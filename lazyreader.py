60dcf1495fe28ef017ba52d27f5c04ab125c491e
Alex Chan <alex@alexwlchan.net>
2017-09-01 23:29:02 +0100

    I don't think specifying the read_size is useful

--- lazyreader.py
+++ lazyreader.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8
 
-def lazyread(f, delimiter, read_size=1024):
+def lazyread(f, delimiter):
     """
     Generator which continually reads ``f`` to the next instance
     of ``delimiter``.
@@ -10,7 +10,6 @@ def lazyread(f, delimiter, read_size=1024):
 
     :param f: Any file-like object which has a ``.read()`` method.
     :param delimiter: Delimiter on which to split up the file.
-    :param read_size: How many bytes to read from the file at a time.
     """
     # Get an empty string to start with.  We need to make sure that if the
     # file is opened in binary mode, we're using byte strings, and similar
@@ -19,7 +18,7 @@ def lazyread(f, delimiter, read_size=1024):
     running = f.read(0)
 
     while True:
-        new_data = f.read(read_size)
+        new_data = f.read(1024)
 
         # When a call to read() returns nothing, we're at the end of the file.
         if not new_data:
@@ -28,7 +27,7 @@ def lazyread(f, delimiter, read_size=1024):
 
         # Otherwise, update the running stream and look for instances of
         # the delimiter.  Remember we might have read more than one delimiter
-        # since the last time we checked.
+        # since the last time we checked
         running += new_data
         while delimiter in running:
             curr, running = running.split(delimiter, 1)
--- test_lazyreader.py
+++ test_lazyreader.py
@@ -1,26 +1,14 @@
 # -*- encoding: utf-8
 
-import random
-import string
-
-import pytest
-
 from lazyreader import lazyread
-
-
-def random_string(size):
-    random.seed(0)
-    return ''.join([random.choice(string.printable) for _ in range(size)])
 
 
 class Readable(object):
     def __init__(self, body):
         self.body = body
         self.position = 0
-        self.read_calls = []
 
     def read(self, size):
-        self.read_calls.append(size)
         retval = self.body[self.position:self.position + size]
         self.position += size
         return retval
@@ -44,10 +32,3 @@ def test_can_read_entire_file():
     ]
     f = Readable(''.join(expected))
     assert list(lazyread(f, delimiter=';')) == expected
-
-
-@pytest.mark.parametrize('read_size', [1, 10, 27])
-def test_reads_file_object_readsize_bytes_at_a_time(read_size):
-    f = Readable(random_string(1000))
-    lazyread(f, delimiter=';', read_size=read_size)
-    assert all([call == read_size for call in f.read_calls])
