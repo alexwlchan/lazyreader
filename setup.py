e422f1bd1b3e9e86a302ab7c50b1d8dea81ace8b
Alex Chan <alex@alexwlchan.net>
2017-09-01 23:05:27 +0100

    Create an installable module

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import codecs
+import os
+import sys
+
+from setuptools import setup
+
+
+# Stolen from Kenneth Reitz
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py register')
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload --universal')
+    sys.exit()
+
+
+def read(f):
+    return codecs.open(f, encoding='utf-8').read()
+
+
+setup(
+    name='lazyreader',
+    version='1.0.0',
+    description='Lazy reading of file objects for efficient batch processing',
+    long_description=read('README.rst'),
+    author='Alex Chan',
+    author_email='alex@alexwlchan.net',
+    url='https://github.com/alexwlchan/lazyreader',
+    py_modules=['lazyreader'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    )
+)
