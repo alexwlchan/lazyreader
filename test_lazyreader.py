a6601dc1d87d681cec9549d07aa5d2ec21e45861
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-06 16:34:09 +0100

    Add a failing test for the boto3 bug

--- test_lazyreader.py
+++ test_lazyreader.py
@@ -1,5 +1,8 @@
 # -*- encoding: utf-8
 
+import boto3
+import moto
+
 from lazyreader import lazyread
 
 
@@ -32,3 +35,13 @@ def test_can_read_entire_file():
     ]
     f = Readable(''.join(expected))
     assert list(lazyread(f, delimiter=';')) == expected
+
+
+@moto.mock_s3
+def test_can_read_from_s3():
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket='bukkit')
+    s3.put_object(Bucket='bukkit', Key='long_file.txt', Body=b'foo\nbar\nbaz')
+
+    f = s3.get_object(Bucket='bukkit', Key='long_file.txt')['Body']
+    assert list(lazyread(f, delimiter=b'\n')) == [b'foo\n', b'bar\n', b'baz']
